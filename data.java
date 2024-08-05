316f4cbc76799f0dff434f894992c3905c5d0235
morenorizqietajauza <moreno_jauza_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 12:50:39 +0700

    Add files via upload

---
+++ data.java
@@ -0,0 +1,14 @@
+public class data {
+    public static void main (String[] args) {
+        //tipe_data nama_var = value/isi var;
+        String nama = "Moreno Rizqieta Jauza";
+        int usia = 16;
+        String alamat = "Danau Ranau II no c21";
+        double kelas = 10.7;
+
+        System.out.println("Nama saya : " + nama);
+        System.out.println("usia : " + usia);
+        System.out.println("alamat : " + alamat);
+        System.out.println("kelas : " + kelas);
+    }
+}
